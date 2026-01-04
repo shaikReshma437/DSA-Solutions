39ae508190be17d4023fa9a9c0d1c9dd932a51a3
shaikReshma437 <rs3237@srmist.edu.in>
2026-01-04 16:35:38 +0530

    Create Array04.java

---
+++ Array04.java
@@ -0,0 +1,10 @@
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] a={5,2,9,1};
+        int min=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]<min) min=a[i];
+        }
+        System.out.println(min);
+    }
+}
