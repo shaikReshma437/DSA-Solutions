87702b63afa8d5918b9eceff8c0751fbc135c4de
shaikReshma437 <rs3237@srmist.edu.in>
2026-01-04 16:36:55 +0530

    Create Array06.java

---
+++ Array06.java
@@ -0,0 +1,9 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] a={10,20,30};
+        int key=20;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==key) System.out.println("Found");
+        }
+    }
+}
