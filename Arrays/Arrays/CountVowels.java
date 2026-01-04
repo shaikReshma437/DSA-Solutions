0e2f22ba68911af197113fddecedf03f857419bb
shaikReshma437 <rs3237@srmist.edu.in>
2026-01-04 16:30:13 +0530

    Create CountVowels.java

---
+++ Arrays/Arrays/CountVowels.java
@@ -0,0 +1,7 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String s="java"; int count=0;
+        for(char c:s.toCharArray()) if("aeiou".indexOf(c)!=-1) count++;
+        System.out.println(count);
+    }
+}
