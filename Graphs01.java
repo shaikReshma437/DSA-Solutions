386b8bea6e86ffee6538a85f571ef46de07a089b
shaikReshma437 <rs3237@srmist.edu.in>
2026-01-04 17:06:51 +0530

    Create Graphs01.java

---
+++ Graphs01.java
@@ -0,0 +1,6 @@
+public class GraphAdjacencyMatrix {
+    public static void main(String[] args){
+        int[][] g = {{0,1,0},{1,0,1},{0,1,0}};
+        System.out.println("Graph adjacency matrix created");
+    }
+}
